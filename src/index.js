f6d873f29e61ab474a77e729ffde5bdd576ca3c9
unknown <york_kila@mail.ru>
2020-10-02 15:28:42 +0300

    init:ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let count = 0;
+  if (!array || array.length === 0) return count;
+  return array.reduce((count, item) => (item < count) ? count = item : count);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let count = 0;
+  if (!array || array.length === 0) return count;
+  return array.reduce((count, item) => (item > count) ? count = item : count);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let count = 0;
+  if (!array || array.length === 0) return count;
+  return array.reduce((count, item) => item + count) / array.length;
 }
